d1e88a988cc6bd59c3f23633bef9c291c5504da6
yandaki <boayves@gmail.com>
2023-08-29 19:21:38 +0000

    Implementation models Category and Product

--- shopping_cart_app/models.py
+++ shopping_cart_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    views = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10 , decimal_places=2)
+    views = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/' , blank=True , null=True)
+    
+    category = models.ManyToManyField(Category)
