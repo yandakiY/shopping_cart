dad024a5235bbea922d4513739fd997961c3aa61
yandaki <boayves@gmail.com>
2023-08-30 14:00:43 +0000

    configuration admin user and interface

--- shopping_cart_app/admin.py
+++ shopping_cart_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product , Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'display_image', 'display_categories')
+    readonly_fields = ('display_image', 'display_categories')
+
+    def display_image(self, obj):
+        return obj.image.url if obj.image else None
+    display_image.short_description = 'Image'
+
+    def display_categories(self, obj):
+        return ', '.join([category.name for category in obj.categories.all()])
+    display_categories.short_description = 'Categories'
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
